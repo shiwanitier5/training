40b5bc4317224d51206919bed4e24f45ffab5e4f
shiwanitier5 <you@example.com>
2020-01-28 16:11:31 +0530

    committed

---
+++ popup.js
@@ -0,0 +1,33 @@
+$(function() {
+    $('#myname').keyup(function() {
+        $('#myid').text('Hello' + $("#myname").val());
+        $('#myid').css("background", "red");
+
+
+
+
+
+
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
